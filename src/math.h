cb33bfd34cb07f1adddc54936e397b5b54999e96
Christian Blume <chr.blume@gmail.com>
2020-04-09 17:10:01 +1200

    std lib independent

--- src/math.h
+++ src/math.h
@@ -5,7 +5,7 @@ namespace mlpga
 
 struct Target
 {
-    enum Type : std::uint8_t
+    enum Type : uint8_t
     {
         Classification,
         Regression,
@@ -27,14 +27,15 @@ struct Target
 namespace detail
 {
 
-inline void min_max(const float* const data,
-                    const std::size_t n,
-                    float& minimum,
-                    float& maximum)
+inline
+void min_max(const float* const data,
+             const size_t n,
+             float& minimum,
+             float& maximum)
 {
     minimum = 1e9f;
     maximum = -1e9f;
-    for (std::size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
         if (data[i] < minimum)
         {
@@ -47,29 +48,32 @@ inline void min_max(const float* const data,
     }
 }
 
-inline void softmax(float* const output, const std::size_t n)
+inline
+void softmax(float* const output, const size_t n)
 {
     float minimum;
     float D;
     min_max(output, n, minimum, D);
     float denom = 1e-6f;
-    for (std::size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
         output[i] -= D; // for numerical stability
         denom += exp(output[i]);
     }
-    for (std::size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
         output[i] = exp(output[i]) / denom;
     }
 }
 
-inline void sigmoid(float& x)
+inline
+void sigmoid(float& x)
 {
     x = 1.0f / (1.0f + exp(-x));
 }
 
-inline void relu(float& x)
+inline
+void relu(float& x)
 {
     if (x < 0.0f)
     {
@@ -77,12 +81,13 @@ inline void relu(float& x)
     }
 }
 
-inline float activate(const float* const weights,
-                      const float* const inputs,
-                      const std::size_t n)
+inline
+float activate(const float* const weights,
+               const float* const inputs,
+               const size_t n)
 {
     float output = 0.0f;
-    for (std::size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
         output += weights[i] * inputs[i];
     }
@@ -90,13 +95,25 @@ inline float activate(const float* const weights,
     return output;
 }
 
+inline
+void copy(const float* const input,
+          const size_t size,
+          float* const output)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        output[i] = input[i];
+    }
+}
+
+inline
 void predict(float* const output,
              const float* const input,
              const Target& network_target,
              const float* const network_weights,
-             const std::size_t n_weights,
-             const std::size_t* const network_layers,
-             const std::size_t n_layers,
+             const size_t n_weights,
+             const size_t* const network_layers,
+             const size_t n_layers,
              float* const in_cache,
              float* const out_cache)
 {
@@ -106,13 +123,13 @@ void predict(float* const output,
     const auto output_size = network_layers[n_layers - 1];
 
     auto weights = network_weights;
-    std::size_t current_size = input_size;
-    std::size_t weight_count = 2u;
+    size_t current_size = input_size;
+    size_t weight_count = 2u;
     const float* source = input;
-    for (std::size_t i = 0; i < n_layers; ++i)
+    for (size_t i = 0; i < n_layers; ++i)
     {
         auto target = i == n_layers - 1 ? output : out_cache;
-        for (std::size_t j = 0; j < network_layers[i]; ++j)
+        for (size_t j = 0; j < network_layers[i]; ++j)
         {
             auto value = detail::activate(weights, source, current_size);
             if (i < n_layers - 1)
@@ -127,10 +144,10 @@ void predict(float* const output,
             current_size = network_layers[i];
             weight_count = current_size + 1;
             source = in_cache;
-            std::copy(target, target + network_layers[i], in_cache);
+            detail::copy(target, network_layers[i], in_cache);
         }
     }
-    assert(weights == network_weights + network_weights + n_weights);
+    assert(weights == network_weights + n_weights);
 
     if (network_target.type == Target::Classification)
     {
@@ -144,11 +161,11 @@ void predict(float* const output,
         }
         const auto factor = network_target.class1 - network_target.class0;
         const auto offset = network_target.class0;
-        std::for_each(output, output + output_size, [factor, offset](float& x)
+        for (size_t i = 0; i < output_size; ++i)
         {
-            x *= factor;
-            x += offset;
-        });
+            output[i] *= factor;
+            output[i] += offset;
+        }
     }
 }
 
